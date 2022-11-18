aeffce13d398ac91b0a649e51582dbda0f475732
Max Vorobyev <chemax05@mail.ru>
2022-11-18 18:38:14 +0300

    delete remove()

--- src/machines/Service.java
+++ src/machines/Service.java
@@ -32,7 +32,6 @@ public class Service {
         for (int i = 0; i < 17; i++) {
             list.add(i);
         }
-        list.remove(12);
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
         }
