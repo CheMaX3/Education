1718de68ad8912d66f6bd590aed37cf3877d6db8
Max Vorobyev <chemax05@mail.ru>
2022-11-18 13:55:21 +0300

    change test

--- src/machines/Service.java
+++ src/machines/Service.java
@@ -32,9 +32,11 @@ public class Service {
         for (int i = 0; i < 17; i++) {
             list.add(i);
         }
+        list.remove(12);
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
         }
+
 
 
         System.out.println("Контрольный ArrayList");
